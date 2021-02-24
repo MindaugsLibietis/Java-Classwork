029b68691ace09c7dae7086687ca6d8ec67f6cee
Mindaugs Libietis <dpikse@gmail.com>
2021-02-24 19:43:06 +0200

    CheckPassFail done

--- src/com/rcs/classwork/Day5/CheckPassFail.java
+++ src/com/rcs/classwork/Day5/CheckPassFail.java
@@ -1,8 +1,18 @@
 package com.rcs.classwork.Day5;
 
+import java.util.Scanner;
+
 public class CheckPassFail {
 
     public static void main(String[] args) {
-	    // write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ludzu ievadiet atzīmi :");
+        int atzime = sc.nextInt();
+        if (atzime >= 50){
+            System.out.println("PASS");
+        }else {
+            System.out.println("FAIL");
+        }
+        System.out.println("DONE");
     }
 }
