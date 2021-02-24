0d51a415b0fa890ea7c30bbeb9f9f6b4a704f5a1
Mindaugs Libietis <dpikse@gmail.com>
2021-02-24 19:55:43 +0200

    OddOrEven

---
+++ src/com/rcs/classwork/Day5/OddOrEven.java
@@ -0,0 +1,16 @@
+package com.rcs.classwork.Day5;
+
+import java.util.Scanner;
+
+public class OddOrEven {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ievadi skaitli :");
+        int skaitlis = sc.nextInt();
+        if (skaitlis % 2 == 0){
+            System.out.println("Para skaitlis");
+        }else{
+            System.out.println("Nepara skaitlis");
+        }
+    }
+}
