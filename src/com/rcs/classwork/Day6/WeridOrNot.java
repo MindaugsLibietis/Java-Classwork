a21eec09ba0b287e1283b9ebc4d900fb89e64e73
Mindaugs Libietis <dpikse@gmail.com>
2021-02-27 11:10:23 +0200

    izlabots

--- src/com/rcs/classwork/Day6/WeridOrNot.java
+++ src/com/rcs/classwork/Day6/WeridOrNot.java
@@ -3,19 +3,17 @@ package com.rcs.classwork.Day6;
 import java.util.Scanner;
 
 public class WeridOrNot {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("ievadi vesalu skaitli: ");
         int sk = sc.nextInt();
         sc.close();
-        if (sk %2 == 1){
+        if (sk % 2 == 1 || sk % 2 == 0 && sk >= 6 && sk <= 20) {
             System.out.println("divaini");
-        }else if (sk %2 == 0 && sk >= 2 && sk <= 5){
+        } else if (sk % 2 == 0 && sk >= 2 && sk <= 5 || sk % 2 == 0 && sk > 20) {
             System.out.println("nav divaini");
-        }else if (sk %2 == 0 && sk >= 6 && sk <= 20){
-            System.out.println("divaini");
-        }else if (sk %2 == 0 && sk > 20){
-            System.out.println("nav divaini");
+        } else {
+            System.out.println("nepareiza ievade");
         }
     }
 }
