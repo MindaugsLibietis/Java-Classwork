42411337c055c2a65cbf370e76a87acc96d31604
Mindaugs Libietis <dpikse@gmail.com>
2021-03-01 22:13:54 +0200

    other ways to do

--- src/com/rcs/classwork/Day7/StringMethods.java
+++ src/com/rcs/classwork/Day7/StringMethods.java
@@ -9,10 +9,18 @@ public class StringMethods {
         String teksts1 = sc.nextLine();
         System.out.println("teksts2 : ");
         String teksts2 = sc.nextLine();
-        if (teksts1.length() < teksts2.length()){
+        /*if (teksts1.length() < teksts2.length()){
             System.out.println(teksts1 + teksts2.toUpperCase() + teksts1);
         }else {
             System.out.println(teksts2 + teksts1.toUpperCase() + teksts2);
-        }
+        }*/
+        
+        /*System.out.println(teksts1.length() >= teksts2.length() ?
+                        teksts2 + teksts1.toUpperCase() + teksts2 :
+                        teksts1 + teksts2.toUpperCase() + teksts1);*/
+
+        String garakais = teksts2.length() > teksts1.length() ? teksts2 : teksts1;
+        String isakais = teksts1.length() < teksts2.length() ? teksts1 : teksts2;
+        System.out.println(isakais + garakais.toUpperCase() + isakais);
     }
 }
