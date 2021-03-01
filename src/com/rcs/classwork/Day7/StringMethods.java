55293719da53e947e9b1e971d6126c8af414d209
Mindaugs Libietis <dpikse@gmail.com>
2021-03-01 19:31:31 +0200

    Klases uzd 1

---
+++ src/com/rcs/classwork/Day7/StringMethods.java
@@ -0,0 +1,20 @@
+package com.rcs.classwork.Day7;
+
+import java.util.Scanner;
+
+public class StringMethods {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("teksts1 : ");
+        String teksts1 = sc.nextLine();
+        System.out.println("teksts2 : ");
+        String teksts2 = sc.nextLine();
+        if (teksts1.length() < teksts2.length()){
+            System.out.println(teksts1 + teksts2.toUpperCase() + teksts1);
+        }else if (teksts2.length() < teksts1.length()){
+            System.out.println(teksts2 + teksts1.toUpperCase() + teksts2);
+        }else {
+            System.out.println("Wrong input");
+        }
+    }
+}
