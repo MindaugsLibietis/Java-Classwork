f3e65d5016c38ffdaaa8f6a4dbd5d277fe1fd74d
Mindaugs Libietis <dpikse@gmail.com>
2021-03-03 19:38:47 +0200

    manor changes third task

--- src/com/rcs/classwork/Day8/WhileLoopThree.java
+++ src/com/rcs/classwork/Day8/WhileLoopThree.java
@@ -3,12 +3,13 @@ package com.rcs.classwork.Day8;
 import java.util.Scanner;
 
 public class WhileLoopThree {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("ludzu ievadiet skaitli : ");
         int sk = sc.nextInt();
+        sc.close();
         int i = 1;
-        while (i <= 10){
+        while (i <= 10) {
             System.out.println(sk + "*" + i + "=" + (sk * i));
             i++;
         }
