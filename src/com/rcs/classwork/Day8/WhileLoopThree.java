dffebe10addb26df585f28444ffc3dc11e1fc35d
Mindaugs Libietis <dpikse@gmail.com>
2021-03-03 19:37:41 +0200

    third task

--- src/com/rcs/classwork/Day8/WhileLoopThree.java
+++ src/com/rcs/classwork/Day8/WhileLoopThree.java
@@ -1,7 +1,16 @@
 package com.rcs.classwork.Day8;
 
+import java.util.Scanner;
+
 public class WhileLoopThree {
     public static void main(String[]args){
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ludzu ievadiet skaitli : ");
+        int sk = sc.nextInt();
+        int i = 1;
+        while (i <= 10){
+            System.out.println(sk + "*" + i + "=" + (sk * i));
+            i++;
+        }
     }
 }
