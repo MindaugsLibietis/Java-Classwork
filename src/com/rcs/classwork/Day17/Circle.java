56d9436668d0772c89486c653b3bcebadf788137
Mindaugs Libietis <dpikse@gmail.com>
2021-03-29 20:54:58 +0300

    printf not working

--- src/com/rcs/classwork/Day17/Circle.java
+++ src/com/rcs/classwork/Day17/Circle.java
@@ -1,2 +1,22 @@
-package com.rcs.classwork.Day17;public class Circle {
+package com.rcs.classwork.Day17;
+
+public class Circle {
+    static final double PI = 3.14159265;
+
+    String Color;
+    double radius;
+
+    public Circle(double rad, String clr){
+        this.Color = "Black";
+        this.radius = rad;
+    }
+    public Circle(double rad){
+        this.radius = rad;
+    }
+    public double getArea() {
+        return PI * radius * radius;
+    }
+    public double getCircleLength() {
+        return 2 * PI * radius;
+    }
 }
--- src/com/rcs/classwork/Day17/Main.java
+++ src/com/rcs/classwork/Day17/Main.java
@@ -1,2 +1,14 @@
-package com.rcs.classwork.Day17;public class Main {
+package com.rcs.classwork.Day17;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle redCircle = new Circle(6.4, "Red");
+        Circle yellowCircle = new Circle(2.0, "Yellow");
+        Circle cyanCircle = new Circle(3.2, "Cyan");
+        Circle blackCircle = new Circle(4.5);
+        System.out.println("RedCircle -> %s, Area = %f, Circle Length = %f\n" redCircle.Color, redCircle.getArea(), redCircle.getCircleLength());
+        System.out.println("YellowCircle -> %s, Area = %f, Circle Length = %f\n" yellowCircle.Color, yellowCircle.getArea(), yellowCircle.getCircleLength());
+        System.out.println("CyanCircle -> %s, Area = %f, Circle Length = %f\n" cyanCircle.Color, cyanCircle.getArea(), cyanCircle.getCircleLength());
+        System.out.println("BlackCircle -> %s, Area = %f, Circle Length = %f\n" blackCircle.Color, blackCircle.getArea(), blackCircle.getCircleLength());
+    }
 }
