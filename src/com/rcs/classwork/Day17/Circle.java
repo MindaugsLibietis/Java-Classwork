0436fbaa01080582a301d8dd342364b21dedc087
Mindaugs Libietis <dpikse@gmail.com>
2021-03-31 19:07:45 +0300

    manor changes

--- src/com/rcs/classwork/Day17/Circle.java
+++ src/com/rcs/classwork/Day17/Circle.java
@@ -2,22 +2,27 @@ package com.rcs.classwork.Day17;
 
 public class Circle {
     static final double PI = 3.14159265;
+    static final String defaultColor = "Black";
 
     String Color;
     double radius;
 
+    public Circle(double rad){
+        this.radius = rad;
+        this.Color = defaultColor;
+    }
     public Circle(double rad, String clr){
         this.radius = rad;
         this.Color = clr;
     }
-    public Circle(double rad){
-        this.radius = rad;
-        this.Color = "Black";
-    }
     public double getArea() {
-        return PI * radius * radius;
+        return PI * this.radius * this.radius;
     }
     public double getCircleLength() {
-        return 2 * PI * radius;
+        return 2 * PI * this.radius;
+    }
+    @Override
+    public String toString(){
+        return String.format("Color %s, Area = %.2f, Circle Length = %.2f\n", this.Color, this.getArea(), this.getCircleLength());
     }
 }
--- src/com/rcs/classwork/Day17/Main.java
+++ src/com/rcs/classwork/Day17/Main.java
@@ -6,9 +6,13 @@ public class Main {
         Circle yellowCircle = new Circle(2.0, "Yellow");
         Circle cyanCircle = new Circle(3.2, "Cyan");
         Circle blackCircle = new Circle(4.5);
-        System.out.printf("RedCircle -> Color %s, Area = %f, Circle Length = %f\n", redCircle.Color, redCircle.getArea(), redCircle.getCircleLength());
-        System.out.printf("YellowCircle -> Color %s, Area = %f, Circle Length = %f\n", yellowCircle.Color, yellowCircle.getArea(), yellowCircle.getCircleLength());
-        System.out.printf("CyanCircle -> Color %s, Area = %f, Circle Length = %f\n", cyanCircle.Color, cyanCircle.getArea(), cyanCircle.getCircleLength());
-        System.out.printf("BlackCircle -> Color %s, Area = %f, Circle Length = %f\n", blackCircle.Color, blackCircle.getArea(), blackCircle.getCircleLength());
+        System.out.print(redCircle);
+        System.out.print(yellowCircle);
+        System.out.print(cyanCircle);
+        System.out.print(blackCircle.toString());
+//        System.out.printf("RedCircle -> Color %s, Area = %.2f, Circle Length = %.2f\n", redCircle.Color, redCircle.getArea(), redCircle.getCircleLength());
+//        System.out.printf("YellowCircle -> Color %s, Area = %.2f, Circle Length = %.2f\n", yellowCircle.Color, yellowCircle.getArea(), yellowCircle.getCircleLength());
+//        System.out.printf("CyanCircle -> Color %s, Area = %.2f, Circle Length = %.2f\n", cyanCircle.Color, cyanCircle.getArea(), cyanCircle.getCircleLength());
+//        System.out.printf("BlackCircle -> Color %s, Area = %.2f, Circle Length = %.2f\n", blackCircle.Color, blackCircle.getArea(), blackCircle.getCircleLength());
     }
 }
